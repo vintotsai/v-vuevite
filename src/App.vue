80bf16ededec4e0ecbf3aaf3aa37e7f5f61648cc
vintotsai <vintotsai@gmail.com>
2022-06-21 22:56:28 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import Ref from './components/Ref.vue'
+export default {
+  components: {
+    HelloWorld,
+    Ref
+  },
+  setup() {
+    return {
+      msg: 'Hello Vue 3 + Vite'
+    }
+  }
+}
+</script>
+
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <Ref />
+  <HelloWorld :msg="msg" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Ref.vue
@@ -0,0 +1,28 @@
+<template>
+  <p class="ref">ref demo: {{name}}, {{age}}</p>
+
+</template>
+<script>
+import { ref, reactive } from 'vue'
+export default {
+  setup() {
+    let name = ref('vue')
+    let age = ref(18)
+    // const state = reactive({
+    //   name,
+    //   age
+    // })
+    // const stateRef = ref(state)
+
+    setTimeout(() => {
+      console.log('setTimeout')
+      name.value = 'vue3'
+    }, 500);
+    return {
+      // stateRef,
+      name,
+      age
+    }
+  }
+}
+</script>
